9c770efde80d1990f1b637c392674c35cdb27601
Jose Guerrero <jguerrero@robinfood.com>
2022-04-08 22:41:25 -0500

    Added context api

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { Route, Routes } from "react-router-dom";
+import { AuthProvider } from "./context/authContext";
 
 import Home from "./components/Home";
 import Login from "./components/Login";
@@ -6,11 +7,13 @@ import Register from "./components/Register";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/register" element={<Register />} />
-    </Routes>
+    <AuthProvider>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </AuthProvider>
   );
 }
 
---
+++ src/context/authContext.js
@@ -0,0 +1,17 @@
+import { createContext, useContext } from "react";
+
+const authContext = createContext();
+
+export const useAuth = () => {
+  const context = useContext(authContext);
+  if (!context) throw new Error("There is no authentication provider");
+  return context;
+};
+
+export function AuthProvider({ children }) {
+  const user = {
+    login: true,
+  };
+
+  return <authContext.Provider value={user}>{children}</authContext.Provider>;
+}
