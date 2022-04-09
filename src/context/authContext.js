eb813d41eceaa40ed305ea9a45ff7c01906699fd
Jose Guerrero <jguerrero@robinfood.com>
2022-04-09 16:38:24 -0500

    Added login user

--- src/context/authContext.js
+++ src/context/authContext.js
@@ -1,5 +1,10 @@
-import { createContext, useContext } from "react";
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import { createContext, useContext, useEffect, useState } from "react";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  onAuthStateChanged,
+  signOut,
+} from "firebase/auth";
 import { auth } from "../firebase";
 
 const authContext = createContext();
@@ -11,10 +16,29 @@ export const useAuth = () => {
 };
 
 export function AuthProvider({ children }) {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const unSubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+      setLoading(false);
+    });
+
+    return () => unSubscribe();
+  }, []);
+
   const signup = (email, password) =>
     createUserWithEmailAndPassword(auth, email, password);
 
+  const login = (email, password) =>
+    signInWithEmailAndPassword(auth, email, password);
+
+  const logout = () => signOut(auth);
+
   return (
-    <authContext.Provider value={{ signup }}>{children}</authContext.Provider>
+    <authContext.Provider value={{ signup, login, user, logout, loading }}>
+      {children}
+    </authContext.Provider>
   );
 }
